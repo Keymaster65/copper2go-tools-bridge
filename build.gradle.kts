0490f8064a6a0de10e80551ee177904d3149e94c
wsluyterman <wolf.sluyterman@scoop-software.de>
2022-01-09 20:11:12 +0100

    Use 3.1.0 of copper2go-api and concrete test dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,16 +46,13 @@ license {
 }
 
 dependencies {
-    implementation("io.github.keymaster65:copper2go-api:3+")
-    implementation("org.copper-engine:copper-coreengine:5+")
-    implementation("org.slf4j:slf4j-api:2.0.0-alpha5")
-    implementation("ch.qos.logback:logback-classic:1.3.0-alpha12")
+    implementation("io.github.keymaster65:copper2go-api:3.1.0")
 
-    testImplementation("org.testcontainers:testcontainers:1.+")
-    testImplementation("org.testcontainers:kafka:1.+")
-    testImplementation("org.assertj:assertj-assertions-generator:2+")
-    testImplementation("net.jqwik:jqwik:1.+")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.+")
-    testImplementation("org.mockito:mockito-core:4+")
-    testImplementation("org.mock-server:mockserver-netty:5.+")
+    testImplementation("org.testcontainers:testcontainers:1.16.2")
+    testImplementation("org.testcontainers:kafka:1.16.2")
+    testImplementation("org.assertj:assertj-assertions-generator:2.2.1")
+    testImplementation("net.jqwik:jqwik:1.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.mockito:mockito-core:4.2.0")
+    testImplementation("org.mock-server:mockserver-netty:5.11.2")
 }
